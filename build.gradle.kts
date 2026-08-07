9e3a01ac50c84006fa498be767a9c261445dadea
icecream <www@withdata.net>
2026-08-07 23:09:36 +0800

    feat: update Kotlin JVM plugin version to 2.4.0 in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    kotlin("jvm") version "2.1.20"
+    kotlin("jvm") version "2.4.0"
     id("org.jetbrains.dokka") version "2.0.0"
     java
     `java-library`
