f2dd0d7204142dd2c5b4dbd91221f50a8bf5272b
icecream <www@withdata.net>
2026-09-11 01:27:29 +0800

    feat: 清理旧的 Spring 实现

--- framework/spring-boot-3-jpa/src/main/kotlin/ako/spring/access/AkoRepository.kt
+++
@@ -1,9 +0,0 @@
-package ako.spring.access
-
-import ako.access.AkoAccess
-import ako.model.db.AkoModel
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface AkoRepository<T : AkoModel> : JpaRepository<T, Int>, AkoAccess<T> {
-
-}
